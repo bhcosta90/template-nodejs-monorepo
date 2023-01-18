be3e9867a7b3bdf7214c8e713cf592c3c4d7dae0
Bruno Costa <bruno.costa@dafiti.com.br>
2023-01-18 15:48:10 -0300

    finalizado

---
+++ src/@core/src/entity/test.entity.spec.ts
@@ -0,0 +1,17 @@
+import { Test } from "./test.entity";
+
+describe("TestEntity Unit Test", () => {
+  beforeAll(
+    () =>
+      (global.console = {
+        ...console,
+        log: jest.fn(),
+      })
+  );
+  
+  it("Constructor", () => {
+    const entity = new Test();
+    expect(entity).toBeInstanceOf(Test);
+    expect(console.log).toBeCalledTimes(1);
+  });
+});
